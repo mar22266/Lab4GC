6d5dfd0852daf1b2b53333281903d3182cfe4cc8
Andre Marroquin <mar22266@uvg.edu.gt>
2024-11-05 15:59:41 -0600

    Feat: Added 2 headers

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+//Dependecias Necesarias
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <sstream>
+#include <vector>
+#include <cassert>
+
+// Headers de las clases Necesarias
+#include "../headers/uniforms.h"
+#include "../headers/shaders.h"
+#include "../headers/fragment.h"
+#include "../headers/triangle.h"
+#include "../headers/camera.h"
+#include "../headers/ObjLoader.h"
+#include "../headers/noise.h"
+#include "../headers/model.h"
+#include "../headers/color.h"
+#include "../headers/print.h"
+#include "../headers/framebuffer.h"
